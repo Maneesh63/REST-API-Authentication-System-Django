b95ba0a1732cd3500f8065fe53fbec09ed3b556a
Maneesh63 <maneeshmaneesh391@gmail.com>
2024-08-02 09:43:53 +0530

    2commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'maneeshmanu929@gmail.com'
-EMAIL_HOST_PASSWORD = 'kdhf xhlo spfj geep '
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your password'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
  
